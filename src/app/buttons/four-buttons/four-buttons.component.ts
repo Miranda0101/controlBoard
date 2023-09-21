5544116df3d8ebb146fb34be0c7d3f98185f97bf
Miranda0101 <xiaomeng.niu@antra.com>
2023-09-21 13:37:30 -0400

    Four buttons updated

--- src/app/buttons/four-buttons/four-buttons.component.ts
+++ src/app/buttons/four-buttons/four-buttons.component.ts
@@ -6,7 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./four-buttons.component.css']
 })
 export class FourButtonsComponent {
+  clickedButtons: boolean[] = [false, false, false, false];
   testClick(num: number){
     console.log(num +  " clicked.");
+    this.clickedButtons[num] = !this.clickedButtons[num];
   }
 }
